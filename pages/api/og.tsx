be95dd3839ae314b672471e8df69d4e46aedc4c8
mb <nakakka@gmail.com>
2022-12-31 13:45:20 +0900

    :recycle: UPDATE：12/31-13:45:20

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,30 +1,68 @@
-import { ImageResponse } from '@vercel/og';
+import { NextRequest } from "next/server";
+import { ImageResponse } from "@vercel/og";
 
 export const config = {
-  runtime: 'edge',
+  runtime: "experimental-edge",
 };
 
-export default function () {
+const font = fetch(new URL("../../public/panda.ttf", import.meta.url)).then(
+  (res) => res.arrayBuffer()
+);
+
+export default async function ogp(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  const fontData = await font;
+  const hasTitle = searchParams.has("title");
+  const title = hasTitle
+    ? searchParams.get("title")?.slice(0, 100)
+    : "Notion Memo";
+
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          textAlign: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
+          backgroundImage: "url(https://ogp.gdgd.tokyo/og-bg-default.png)",
+          backgroundColor: "#fff",
+          backgroundSize: "100% 100%",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          flexDirection: "column",
+          flexWrap: "nowrap",
         }}
       >
-        Hello world!
+        <div
+          style={{
+            width: "100%",
+            fontSize: 60,
+            fontStyle: "normal",
+            fontWeight: "bold",
+            color: "#fff",
+            padding: "0 120px",
+            lineHeight: 1.8,
+            justifyContent: "center",
+            marginBottom: "30px",
+            wordWrap: "break-word",
+          }}
+        >
+          {title}
+        </div>
       </div>
     ),
     {
       width: 1200,
-      height: 600,
-    },
+      height: 630,
+      emoji: "twemoji",
+      fonts: [
+        {
+          name: "panda",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
   );
 }
